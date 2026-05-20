66150180c74ae5849001fa56f7594c97cbacecdd
motherchod-code <sreejanpatra@gmail.com>
2026-05-20 22:18:51 +0530

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -3,7 +3,7 @@ import yts from "yt-search";
 import { Module } from "../lib/plugins.js";
 
 Module({
-  command: "play",
+  command: "plays",
   package: "youtube",
   description: "Play song from YouTube (API based)",
 })(async (message, match) => {
